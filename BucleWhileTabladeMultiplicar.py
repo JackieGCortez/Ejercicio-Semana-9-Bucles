1b8550875529c7c605bfdb37be407eb8a0055461
JackieGCortez <gc1496042021@unab.edu.sv>
2021-09-25 09:56:12 -0600

    actualizacion

---
+++ BucleWhileTabladeMultiplicar.py
@@ -0,0 +1,9 @@
+
+inicio = 1
+
+
+while(inicio<=30):
+    tabla = 6*inicio
+    print("6", "* ", inicio , " = " , tabla)
+    inicio+=1
+    
---
+++ EjercicioSemana9.py
@@ -0,0 +1,8 @@
+
+inicio =0
+
+while(inicio<=100):
+    if(inicio%3==0):  
+        print(inicio, "Numeros Divisbles por 3")
+    inicio+=1
+    
